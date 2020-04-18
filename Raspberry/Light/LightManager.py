33832e815cdbdb4b567da2b32a9ea744fde13d38
Altamir Bitencourt Junior <altamir.bitencourt@etec.sp.gov.br>
2020-04-18 17:21:28 -0300

    Added Commands to Turn on and Turn off light

---
+++ Raspberry/Light/LightManager.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+
+class LightManager(object):
+
+    def __init__(self, number_output):
+        self._number_output = number_output
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(number_output, GPIO.OUT)
+
+    def turn_on_light(self):
+        GPIO.output(self._number_output, 1)
+        return
+
+    def turn_off_light(self):
+        GPIO.output(self._number_output, 0)
+        return
+
---
+++ Raspberry/Main.py
@@ -0,0 +1,5 @@
+
+from Raspberry.Light.LightManager import LightManager
+
+light = LightManager(12)
+light.turn_off_light()
