99540e2ad5d9b1698a9ab682e4539d063b662c9b
Altamir Bitencourt Junior <altamir.bitencourt@etec.sp.gov.br>
2020-04-19 02:00:14 -0300

    Update main

--- Raspberry/Main.py
+++ Raspberry/Main.py
@@ -1,6 +1,13 @@
 
+import time
+
 from Raspberry.Light.LightManager import LightManager
 
 light = LightManager(2)
-light.turn_off_light()
+
+while True:
+    light.turn_off_light()
+    time.sleep(2)
+    light.turn_on_light()
+
 
